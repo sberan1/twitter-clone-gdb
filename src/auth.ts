c4b366910b0f478a4232e681245e45b7023e8c0a
sberan1 <34517803+sberan1@users.noreply.github.com>
2024-12-31 21:56:16 +0100

    Auth - remade and created a screen

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,8 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/sveltekit/providers/github"
-import { AUTH_SECRET, AUTH_GITHUB_SECRET, AUTH_GITHUB_ID, NEO4J_PASSWORD, NEO4J_USER, NEO4J_URI} from "$env/static/private"
+import Discord from "@auth/sveltekit/providers/discord"
+import Credentials from "@auth/sveltekit/providers/credentials"
+import { AUTH_SECRET, AUTH_GITHUB_SECRET, AUTH_GITHUB_ID, NEO4J_PASSWORD, NEO4J_USER, NEO4J_URI, AUTH_DISCORD_ID, AUTH_DISCORD_SECRET} from "$env/static/private"
 import neo4j from "neo4j-driver"
 import { Neo4jAdapter} from '@auth/neo4j-adapter'
 
@@ -18,6 +20,42 @@ export const { handle, signIn, signOut } = SvelteKitAuth(async () => {
 			GitHub({
 				clientId: AUTH_GITHUB_ID,
 				clientSecret: AUTH_GITHUB_SECRET
+			}),
+			Discord({
+				clientId: AUTH_DISCORD_ID,
+				clientSecret: AUTH_DISCORD_SECRET
+			}),
+			Credentials({
+				credentials: {
+					name: {},
+					email: {},
+					password: {},
+				},
+				authorize: async (credentials) => {
+					if (!credentials) {
+						throw new Error("No credentials provided");
+					}
+
+					const { email, password, name } = credentials;
+
+					if (!email || !password) {
+						throw new Error("Email and password are required");
+					}
+
+					const pwHash = saltAndHashPassword(password);
+
+					let user = await getUserFromDb(email, pwHash);
+
+					if (!user) {
+						user = await registerUser(email, name, pwHash);
+					}
+
+					if (!user) {
+						throw new Error("Invalid credentials");
+					}
+
+					return user;
+				}
 			})
 		],
 		secret: AUTH_SECRET,
@@ -25,3 +63,44 @@ export const { handle, signIn, signOut } = SvelteKitAuth(async () => {
 	}
 	return authOptions
 })
+
+function saltAndHashPassword(password: string) {
+	// logic to salt and hash password
+	return password
+}
+
+async function getUserFromDb(email, pwHash) {
+	// logic to verify if the user exists
+	const result = await neo4jSession.run(
+		'MATCH (u:User {email: $email, pwHash: $pwHash}) RETURN u',
+		{ email, pwHash }
+	);
+	if (result.records.length === 0) {
+		return null;
+	}
+	const user = result.records[0].get('u').properties;
+	return {
+		id: user.id,
+		name: user.name,
+		email: user.email,
+	}
+}
+
+async function registerUser(email, name, pwHash) {
+	// logic to create a new user
+	const result = await neo4jSession.run(
+		'CREATE (u:User {email: $email, name: $name, pwHash: $pwHash}) RETURN u',
+		{ email, name, pwHash }
+	);
+
+	if (result.records.length === 0) {
+		throw new Error('User registration failed');
+	}
+
+	const user = result.records[0].get('u').properties;
+	return {
+		id: user.id,
+		name: user.name,
+		email: user.email,
+	}
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,58 @@
 <script>
-	import { signIn, signOut } from "@auth/sveltekit/client"
+	import { signIn } from "@auth/sveltekit/client"
+	import { Button } from "$lib/components/ui/button/index.ts";
+	import * as Card from "$lib/components/ui/card/index.ts";
+	import { Label } from "$lib/components/ui/label/index.ts";
+	import { Input } from "$lib/components/ui/input/index.ts";
+	import { Icons } from "$lib/components/docs/icons/index.ts";
+
+
+	let password = "";
+	let email = "";
+	let name = "";
 </script>
 
-<button on:click={() => signIn('github')} class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Sign in with Github</button>
-<button on:click={() => signOut()} class="signIn btn btn-blue">Sign out</button>
+<div class="flex flex-col items-center justify-center h-screen">
+<Card.Root>
+	<Card.Header class="space-y-1">
+		<Card.Title class="text-2xl">Sign In!</Card.Title>
+		<Card.Description>Enter your email below to create or login to your account</Card.Description>
+	</Card.Header>
+	<Card.Content class="grid gap-4">
+		<div class="grid grid-cols-2 gap-6">
+			<Button variant="outline" on:click={() => signIn('github')}>
+				<Icons.gitHub class="mr-2 h-4 w-4" />
+				GitHub
+			</Button>
+			<Button variant="outline" on:click={() => signIn('discord')}>
+				<Icons.discord class="mr-2 h-4 w-4" />
+				Discord
+			</Button>
+		</div>
+		<div class="relative">
+			<div class="absolute inset-0 flex items-center">
+				<span class="w-full border-t"> </span>
+			</div>
+			<div class="relative flex justify-center text-xs uppercase">
+				<span class="bg-card text-muted-foreground px-2"> Or continue with </span>
+			</div>
+		</div>
+		<div class="grid gap-2">
+			<Label for="name">Full Name</Label>
+			<Input id="name" type="text" placeholder="John Doe" bind:value={name} />
+		</div>
+		<div class="grid gap-2">
+			<Label for="email">Email</Label>
+			<Input id="email" type="email" placeholder="m@example.com" bind:value={email}/>
+		</div>
+		<div class="grid gap-2">
+			<Label for="password">Password</Label>
+			<Input id="password" type="password" placeholder="****" bind:value={password}/>
+		</div>
+	</Card.Content>
+	<Card.Footer>
+		<Button class="w-full" on:click={() => signIn("credentials", { name, email, password })}>Create account</Button>
+	</Card.Footer>
+</Card.Root>
+</div>
 
-<style>
-	.btn {
-			display: inline-block;
-	}
-</style>
